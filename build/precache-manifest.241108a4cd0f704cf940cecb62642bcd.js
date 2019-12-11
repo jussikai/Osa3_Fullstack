6cfa324f887bc1c994575e8e017a3a6e5b7f35ed
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-12-11 23:52:33 +0200

    fix: front paths

---
+++ build/precache-manifest.241108a4cd0f704cf940cecb62642bcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa9d09b5d930850c6d2b6fe1fccba0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22ab9e627813dac266e2",
+    "url": "/static/css/main.fa3552b7.chunk.css"
+  },
+  {
+    "revision": "897745daf7f8a8f969be",
+    "url": "/static/js/2.bbdc062d.chunk.js"
+  },
+  {
+    "revision": "22ab9e627813dac266e2",
+    "url": "/static/js/main.663963ab.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
