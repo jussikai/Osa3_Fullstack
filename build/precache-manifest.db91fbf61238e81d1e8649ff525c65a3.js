f0ae770744f66bfeb39c3422a9973f718aa6b795
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-12-11 23:30:03 +0200

    add: added remaining build files

---
+++ build/precache-manifest.db91fbf61238e81d1e8649ff525c65a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f30c143c3e50d49b7c0906d50aa507f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6ac8b643cc657af95a",
+    "url": "/static/css/main.fa3552b7.chunk.css"
+  },
+  {
+    "revision": "897745daf7f8a8f969be",
+    "url": "/static/js/2.bbdc062d.chunk.js"
+  },
+  {
+    "revision": "ba6ac8b643cc657af95a",
+    "url": "/static/js/main.8f018736.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
